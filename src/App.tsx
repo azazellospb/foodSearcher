b1181127b211497f283be33a7f5810e493418cbf
Anton Ivanov <aivanov.mcu@gmail.com>
2022-09-21 11:45:26 +0300

    feat: add suspense/lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {
   Routes,
   Route,
@@ -6,28 +6,31 @@ import {
 import './App.css';
 import Layout from './components/Layout';
 import { Home } from './pages/Home';
-import { Search } from './pages/Search';
 import { History } from './pages/History';
 import { Favorites } from './pages/Favorites';
 import { Signin } from './pages/Signin';
 import { Signup } from './pages/Signup';
-import { Recipe } from './pages/Recipe';
 import { ErrorBoundary } from './components/ErrorBoundary';
+
+const Search = lazy(() => import('./pages/Search'));
+const Recipe = lazy(() => import('./pages/Recipe'));
 
 function App() {
   return (
     <ErrorBoundary>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/recipe" element={<Recipe />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/recipe" element={<Recipe />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </ErrorBoundary>
   );
 }
